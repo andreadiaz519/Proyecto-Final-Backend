51815fa9ae5c420c499bf8d37e244cfc18dba513
Andrea Diaz <andreadiaz_2308@hotmail.com>
2026-05-06 16:38:14 -0500

    error con middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,10 +1,13 @@
 import jwt from "jsonwebtoken";
 
 const verifyToken = (req, res, next) => {
+
   try {
+
     const authHeader = req.headers.authorization;
 
     if (!authHeader) {
+
       return res.status(401).json({
         mensaje: "No hay token",
       });
@@ -13,17 +16,23 @@ const verifyToken = (req, res, next) => {
     const token = authHeader.split(" ")[1];
 
     if (!token) {
+
       return res.status(401).json({
         mensaje: "Token inválido",
       });
     }
 
-    const verified = jwt.verify(token, "secreto123");
+    const verified = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
 
     req.user = verified;
 
     next();
+
   } catch (error) {
+
     return res.status(401).json({
       mensaje: "Token inválido",
     });
